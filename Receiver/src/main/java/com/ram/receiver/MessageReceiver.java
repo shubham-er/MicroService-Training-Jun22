e8bb1af4d21658595be0861c7c5f6c92b9b06b03
shubham-er <ershubham@ymail.com>
2022-06-17 14:18:50 +0530

    active mq

---
+++ Receiver/src/main/java/com/ram/receiver/MessageReceiver.java
@@ -0,0 +1,23 @@
+package com.ram.receiver;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import com.ram.model.Product;
+
+@Component
+public class MessageReceiver {
+	private static final String MESSAGE_QUEUE = "message_queue";
+
+	@JmsListener(destination = MESSAGE_QUEUE)
+	public void receiveMessage(Product product) {
+		System.out.println("Received " + product);
+//		throwexception(product);
+	}
+
+	private void throwexception(Product product) {
+		if (product.getQuantity() % 2 == 0) {
+			throw new RuntimeException();
+		}
+	}
+}
